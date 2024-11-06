b735f5af9808bc66ce29bfc3186d71c5f7799660
rahadityaputra <rahadit.a.p123@gmail.com>
2024-11-06 15:55:18 +0700

    mengubah contoh config env

--- src/configuration/database.js
+++ src/configuration/database.js
@@ -1,10 +1,10 @@
 import mysql from 'mysql2/promise';
 
 const connection = await mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'gadgetlab_database'
+    host: process.env.HOSTNAME,
+    user: process.env.USERNAME,
+    password: process.env.PASSWORD || "",
+    database: process.env.DATABASE_NAME
 });
 
 
