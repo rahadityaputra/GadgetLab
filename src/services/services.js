cce2d3d4185492967b5f38751cd243968f68ba86
rahadityaputra <rahadit.a.p123@gmail.com>
2024-11-13 15:32:56 +0700

    ubah halaman phone

--- src/services/services.js
+++ src/services/services.js
@@ -5,10 +5,24 @@ import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const cacheFilePath = path.join(__dirname, "cache", "topDevices.json");
+
+
+const findDevice = (devices = [], id) => {
+  if (devices) {
+    for (let i = 0; i < devices.length; i++) {
+      if (devices[i].id == id) {
+        return devices[i];
+      }
+    }
+    return null;
+  }
+
+  return null;
+}
 
 // Fungsi untuk memuat cache dari file JSON
 const loadCache = async () => {
+  const cacheFilePath = path.join(__dirname, "cache", "topDevices.json");
   try {
     const data = await fs.readFile(cacheFilePath, "utf-8");
     const parsedData = JSON.parse(data);
@@ -24,7 +38,26 @@ const loadCache = async () => {
   return null; // Jika tidak ada cache atau sudah kadaluarsa
 };
 
-const saveCache = async (devices) => {
+// Fungsi untuk memuat cache dari file JSON
+const loadCacheDevices = async () => {
+  const cacheFilePath = path.join(__dirname, "cache", "devices.json");
+  try {
+    const data = await fs.readFile(cacheFilePath, "utf-8");
+    const parsedData = JSON.parse(data);
+
+    // Cek apakah cache masih valid (misalnya 24 jam)
+    const oneDay = 24 * 60 * 60 * 1000; // dalam milidetik
+    if (Date.now() - parsedData.timestamp < oneDay) {
+      return parsedData.devices;
+    }
+  } catch (error) {
+    console.log("Cache tidak tersedia atau sudah kadaluarsa.");
+  }
+  return null; // Jika tidak ada cache atau sudah kadaluarsa
+};
+
+const saveCacheTopPhones = async (devices) => {
+  const cacheFilePath = path.join(__dirname, "cache", `topDevices.json`);
   const data = {
     timestamp: Date.now(),
     devices,
@@ -32,6 +65,21 @@ const saveCache = async (devices) => {
   await fs.mkdir(path.dirname(cacheFilePath), { recursive: true });
   await fs.writeFile(cacheFilePath, JSON.stringify(data), "utf-8");
 };
+
+const saveCacheDevice = async (device) => {
+  const cacheFilePath = path.join(__dirname, "cache", `devices.json`);
+  const devices = await loadCacheDevices() || []; 
+  devices.push(device);
+  const data = {
+    timestamp: Date.now(),
+    devices,
+  };
+
+  await fs.mkdir(path.dirname(cacheFilePath), { recursive: true });
+  await fs.writeFile(cacheFilePath, JSON.stringify(data), "utf-8");
+};
+
+
 
 export const getDeviceList = async () => {
   let devices = await loadCache();
@@ -65,7 +113,7 @@ export const getDeviceList = async () => {
       })
     );
 
-    await saveCache({ topPhonesByFansList, topPhonesByInterestList });
+    await saveCacheTopPhones({ topPhonesByFansList, topPhonesByInterestList });
 
     return {
       topPhonesByFansList,
@@ -79,13 +127,25 @@ export const getDeviceList = async () => {
 
 
 export const getDeviceDetail = async (id) => {
-  try {
-    const phone = await gsmarena.catalog.getDevice(id);
-    return phone;
-  } catch (error) {
-    return {
-      message : error
+  // sebelum request ke api maka cari dulu di devices.json 
+  const devices = await loadCacheDevices();
+  const device = findDevice(devices, id);
+  if (device) {
+    return device;
+  } else {
+    try {
+      const device = await gsmarena.catalog.getDevice(id);
+      // jika sudah req ke api maka tambah ke cache nya
+      device.id = id;
+      await saveCacheDevice(device);
+      return device;
+    } catch (error) {
+      return {
+        message : error
+      }
     }
   }
+
   
 }
+
